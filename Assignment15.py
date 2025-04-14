4426d39d24366dd104f1a14bea270dd948497b93
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-15 00:28:31 +0530

    Add files via upload

---
+++ Assignment15.py
@@ -0,0 +1,31 @@
+#Q1
+i=0
+while i<5:
+    print("MysirG")
+    i+=1
+print()
+#Q2
+i=1
+while i<11:
+    print(i)
+    i+=1
+print()
+#Q3
+i=10
+while i>0:
+    print(i)
+    i-=1
+print()
+#Q4
+i=0
+while i<10:
+    print(2*i+1)
+    i+=1
+print()
+#Q5
+i=10
+while i>0:
+    print(2*i-1)
+    i-=1
+
+
